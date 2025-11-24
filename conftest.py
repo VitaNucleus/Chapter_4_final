abe03ea74b14aa757174e6a9c54bd417ba808b56
Daniil <127922540+VitaNucleus@users.noreply.github.com>
2025-11-24 10:51:42 +0500

    Chapter 4. Begin and first files

---
+++ conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--language',
+        action='store',
+        default='en',
+        help='Choose language'
+        )
+
+@pytest.fixture()
+def browser(request):
+    user_language = request.config.getoption("language")
+    print("\nstart chrome browser for test..")
+    options = Options()
+    options.add_argument(f'lang={user_language}')
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
